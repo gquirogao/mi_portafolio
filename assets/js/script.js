37d24f3196f5f1da5186c192bd98750f18d48c3b
Guada <gquirogaoliva@gmail.com>
2020-05-26 16:04:41 -0300

    ultima version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
-$(document).ready(function(){
-
+$(function (){
 	$("a").click(function(event){
-		if (this.hash !== "") {
-			event.preventDefault ();
+		if(this.hash !== "") {
+			event.preventDefault();
 
 			var gato = this.hash;
 
-			$("html, body").animate({
+			$("html, body").animate ({
 				scrollTop: $(gato).offset().top
-			}, 800, function(){
+			},850, function() {
 				window.location.hash = gato;
-			});
 
+		});
+	}
+});
+});
+
+$(document).ready(function(){
+  $(".titulo").hover(function(){
+    $(this).css("color", "#232424");
+    }, function(){
+    $(this).css("color", "#9c9c9c");
+  });
+});
